94f88e683593f27fd21e953414c8dae259568e4c
FullSoYu <hey6410@naver.com>
2022-08-03 12:01:35 +0900

    22강 react todolist todos 추가

--- src/App.js
+++ src/App.js
@@ -9,17 +9,37 @@ function App() {
     setTodo(e.target.value);
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo === "") {
+      alert("한글자 이상 등록");
+      return;
+    }
+
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
   return (
     <div>
       <h1>할 일</h1>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           onChange={onChange}
           value={todo}
           placeholder="할 일을 적어주세요."
         />
+        <button>등록</button>
       </form>
+      <button
+        onClick={() => {
+          console.log(todos);
+        }}
+      >
+        check
+      </button>
     </div>
   );
 }

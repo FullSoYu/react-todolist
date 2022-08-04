4de59a0cb974bb86afe652d271e60dadb881f0e5
FullSoYu <hey6410@naver.com>
2022-08-05 00:01:53 +0900

    24강 todolist 리뉴얼 todo 추가

--- src/App.js
+++ src/App.js
@@ -1,52 +1,31 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import TodoInsert from "./components/TodoInsert";
+import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
   const [todos, setTodos] = useState([]);
-
-  const [todo, setTodo] = useState("");
-
-  const onChange = (e) => {
-    setTodo(e.target.value);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (todo === "") {
-      alert("한글자 이상 입력하세요.");
-      return;
-    }
-
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
+  const nextId = useRef(1);
+  const onInsert = (text) => {
+    const todo = {
+      id: nextId,
+      text: text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId++;
   };
 
   return (
-    <div>
-      <h1>할 일</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          onChange={onChange}
-          value={todo}
-          placeholder="할 일을 적어주세요."
-        />
-        <button>등록</button>
-      </form>
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
       <button
         onClick={() => {
           console.log(todos);
         }}
       >
-        check
+        show todos
       </button>
-      <hr />
-      <ul>
-        {todos.map((todo) => (
-          <li>{todo}</li>
-        ))}
-      </ul>
-    </div>
+    </TodoTemplate>
   );
 }
 
